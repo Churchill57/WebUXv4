a1ab859da77c5e56dbf6376220cd9687f8ed31ec
Churchill57 <JohnHutson@ExeDev.co.uk>
2019-02-20 17:18:56 +0000

    Created GOLD.Portal.MVC with SQL Server Identity database. Tables placed in GOLD.AppExecution, which was imported into GOLD.AppExecution.Database project.

---
+++ GOLD.Portal.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GOLD.Portal.MVC.Startup))]
+namespace GOLD.Portal.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
